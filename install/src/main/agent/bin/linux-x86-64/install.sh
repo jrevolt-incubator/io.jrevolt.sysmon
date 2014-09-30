b606faf186746c1a19e28b454d24ffa0000e7ef1
Patrik Beno <patrik.beno@greenhorn.sk>
2014-09-30 22:03:26 +0200

    install

---
+++ install/src/main/agent/bin/linux-x86-64/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -eu
+fail() { echo "$1"; exit 1; }
+[ $(whoami) = root ] || fail "Must be root, not $(whoami)";
+
+dir="$(realpath $(dirname $0))"
+
+cp -v $dir/sysmon-agent.conf /etc/sysconfig/sysmon-agent && chown sysmon:sysmon /etc/sysconfig/sysmon-agent
+rm -fv /etc/init.d/sysmon-agent || true
+ln -s $dir/sysmon-agent /etc/init.d/sysmon-agent && (
+	chown root:root $dir/sysmon-agent;
+	chmod 755 $dir/sysmon-agent;
+)
+chkconfig sysmon-agent on
+
+service sysmon-agent start
---
+++ install/src/main/server/bin/linux-x86-64/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -eu
+fail() { echo "$1"; exit 1; }
+[ $(whoami) = root ] || fail "Must be root, not $(whoami)";
+
+# cleanup/reset
+service sysmon-agent stop || true
+chown -R sysmon:sysmon /opt/sysmon
+
+dir="$(realpath $(dirname $0))"
+
+cp -v $dir/sysmon-server.conf /etc/sysconfig/sysmon-server
+rm -fv /etc/init.d/sysmon-server || true
+ln -s $dir/sysmon-server /etc/init.d/sysmon-server && (
+	chown root:root $dir/sysmon-server;
+	chmod 755 $dir/sysmon-server;
+)
+chkconfig sysmon-server on
+
+service sysmon-server start

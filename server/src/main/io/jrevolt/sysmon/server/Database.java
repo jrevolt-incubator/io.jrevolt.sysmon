aa393574adc70c3e3d2be1025097d69e92d8969f
Patrik Beno <patrik.beno@greenhorn.sk>
2014-10-02 07:41:11 +0200

    server in-mem database: listener support

--- server/src/main/io/jrevolt/sysmon/server/Database.java
+++ server/src/main/io/jrevolt/sysmon/server/Database.java
@@ -1,8 +1,18 @@
 package io.jrevolt.sysmon.server;
 
+import io.jrevolt.sysmon.model.AgentInfo;
+import io.jrevolt.sysmon.model.DomainDef;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import javax.inject.Singleton;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author <a href="mailto:patrikbeno@gmail.com">Patrik Beno</a>
@@ -10,7 +20,47 @@ import javax.inject.Singleton;
 @Service @Singleton
 public class Database {
 
+	@Autowired
+	DomainDef domain;
 
+	private Map<String,AgentInfo> agents = new HashMap<>();
 
+	@PostConstruct
+	void init() {
+		domain.getClusters().values().forEach(c->{
+			c.getServers().forEach(s-> {
+				agents.put(s, new AgentInfo(c.getName(), s, AgentInfo.Status.UNKNOWN, null, null));
+			});
+		});
+	}
 
+	public Map<String, AgentInfo> getAgents() {
+		return agents;
+	}
+
+	public void updateAgent(AgentInfo info) {
+		agents.get(info.getServer()).updateFrom(info);
+		fireUpdate(info.getServer());
+	}
+
+	///
+
+	final Map<String, Set<Runnable>> listeners = new ConcurrentHashMap<>();
+
+	public void onUpdate(String serverName, Runnable r) {
+		synchronized (listeners) {
+			Set<Runnable> jobs = listeners.get(serverName);
+			if (jobs == null) {
+				jobs = new HashSet<>();
+				listeners.put(serverName, jobs);
+			}
+			jobs.add(r);
+		}
+	}
+
+	void fireUpdate(String serverName) {
+		Set<Runnable> jobs = listeners.remove(serverName);
+		if (jobs == null) { return; }
+		jobs.parallelStream().forEach(Runnable::run);
+	}
 }

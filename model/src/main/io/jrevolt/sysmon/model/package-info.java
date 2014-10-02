23c5f4ed867a05f55c2f8c08cab2efba79c6ed32
Patrik Beno <patrik.beno@greenhorn.sk>
2014-10-03 00:10:53 +0200

    more converters

--- model/src/main/io/jrevolt/sysmon/model/package-info.java
+++ model/src/main/io/jrevolt/sysmon/model/package-info.java
@@ -1,10 +1,16 @@
-@XmlJavaTypeAdapters(
-		@XmlJavaTypeAdapter(type=Instant.class, value=InstantConverter.class)
-)
+@XmlJavaTypeAdapters({
+		@XmlJavaTypeAdapter(type=Instant.class, value=InstantConverter.class),
+		@XmlJavaTypeAdapter(type=Period.class, value=PeriodConverter.class),
+		@XmlJavaTypeAdapter(type=Duration.class, value=DurationConverter.class)
+})
 package io.jrevolt.sysmon.model;
 
+import io.jrevolt.sysmon.common.DurationConverter;
 import io.jrevolt.sysmon.common.InstantConverter;
+import io.jrevolt.sysmon.common.PeriodConverter;
 
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapters;
+import java.time.Duration;
 import java.time.Instant;
+import java.time.Period;

42fd00acf78691513cb9ac1f7fc09a840272f981
Patrik Beno <patrik.beno@greenhorn.sk>
2014-10-23 12:06:59 +0200

    deploy all agents

--- install/src/main/agent/bin/deploy_all.sh
+++ install/src/main/agent/bin/deploy_all.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-set -eu
-
 cd $(dirname $0)
 
-server_url="$1"
+test="
+$(echo st1{soa,ints,bpms,crms}{01,02})
+$(echo st1prox{01,02,11,12})
+$(echo st1port{01,02,11,12})
+$(echo st1epod{01,02,11,12})
+$(echo st1bils{01,02})
+$(echo st1lear{01,02})
+$(echo st1ccas{01,02,03,04})
+$(echo st1vu{as,ss}{01,02})
+$(echo st1egov{01,02,11,12,21,22,31,32,41,42,51,52})
+$(echo st1sts{01,02})
+"
 
-curl -s "$server_url/rest/servers"
-
-echo TODO
+for i in $test; do
+echo "## $i ##"
+./deploy.sh $i
+done

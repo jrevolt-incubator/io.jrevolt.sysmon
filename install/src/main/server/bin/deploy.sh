7d2563db22725f6da57137e788d87662f7ae1026
Patrik Beno <patrik.beno@greenhorn.sk>
2014-12-11 23:49:55 +0100

    deploy server (script update)

--- install/src/main/server/bin/deploy.sh
+++ install/src/main/server/bin/deploy.sh
@@ -12,4 +12,4 @@ cd $(dirname $0)/..
 
 tar cz . | ssh $server sudo -u sysmon tar xz -C /opt/sysmon/
 
-ssh $server sudo /opt/sysmon/bin/linux-x86-64/install.sh\; sudo service $name status
+ssh -t $server sudo /opt/sysmon/bin/linux-x86-64/install.sh\; sudo service $name status
